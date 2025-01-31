9d705378fa52b943428c4516a7d4278d7b77c72d
bgshim <lanore@gmail.com>
2025-01-31 13:16:40 +0900

    prisma 초기설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from 'next/font/google';
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +17,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-neutral-900 text-white max-w-screen-sm mx-auto`}>
+        className={`${inter.className} bg-neutral-900 text-white max-w-screen-sm mx-auto`}
+      >
         {children}
       </body>
     </html>
